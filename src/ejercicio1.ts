3045756e84f391f511bfc640e42821293fde6bbe
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-04-26 22:11:53 -0300

    Primeras practicas ejercicio if/else ejercicio compra con descuento

---
+++ src/ejercicio1.ts
@@ -0,0 +1,14 @@
+let numero1 : number = Number (prompt("ingresa un numero"));
+ if (numero1 >= 20) { 
+  console.log("el numero es mayor")
+ } else {
+   console.log("el numero es menor")
+ }
+
+
+ 
+ 
+
+
+
+
---
+++ src/ejercicio2.ts
@@ -0,0 +1,15 @@
+if (7 > 4 && 20 > 10) {
+  console.log("verdadero");
+} else {
+  console.log("falso");
+}
+
+if (7 > 4 || 20 < 10) {
+  console.log("verdadero");
+} else {
+  console.log("false");
+}
+
+if (20 > 10 || !20 < 10) {
+  console.log("el primero es correcto");
+}
