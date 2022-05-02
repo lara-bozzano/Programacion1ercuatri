a3db8249ed2a9d7b7b0338a52bd2ec2348d9c2d2
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-05-02 19:53:05 -0300

    ejercicio suma

---
+++ src/ciclos.ts
@@ -0,0 +1,14 @@
+let numero1: number = Number(prompt("Ingrese primer numero"));
+let numero2: number = Number(prompt("Ingrese segundo numero"));
+let suma = 0;
+if (numero1 <= numero2) {
+  for (let indice: number = numero1; indice <= numero2; indice++) {
+    suma = suma + indice;
+    console.log("Su resultado es", suma);
+  }
+} else {
+  for (let indice2: number = numero2; indice2 <= numero1; indice2++) {
+    suma = suma + indice2;
+    console.log("Su resultado es", suma);
+  }
+}
