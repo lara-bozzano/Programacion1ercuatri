44403df81410c1caf58c27f71fb48341e0103a5f
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-05-02 23:38:36 -0300

    ejercicio suma

--- src/suma.ts
+++ src/suma.ts
@@ -4,11 +4,10 @@ let suma = 0;
 if (numero1 <= numero2) {
   for (let indice: number = numero1; indice <= numero2; indice++) {
     suma = suma + indice;
-    console.log("Su resultado es", suma);
   }
 } else {
   for (let indice2: number = numero2; indice2 <= numero1; indice2++) {
     suma = suma + indice2;
-    console.log("Su resultado es", suma);
   }
 }
+console.log("Su resultado es", suma);
