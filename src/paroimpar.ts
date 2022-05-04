cfe58bb22575453c82e02d855667f2be6f725e18
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-05-03 22:12:59 -0300

    ejercicio es par

---
+++ src/paroimpar.ts
@@ -0,0 +1,15 @@
+//Desarrollar un algorit 
+let numero: number = Number(prompt("Ingrese numero"));
+
+switch numero {
+
+  case numero % 2 === 0:
+  console.log("El numero es par")
+  break;
+  case numero % 2 != 0:
+    console.log("El numero es impar")
+//    default:
+//      console.log("")
+} 
+
+
