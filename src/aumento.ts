ace7256a5e95f484f088a5e69139ea17422a4522
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-05-08 13:15:45 -0300

    ejercicio encontrar max y min

--- src/aumento.ts
+++ src/aumento.ts
@@ -4,13 +4,14 @@ let nuevoSueldo: number = 0
 if (0 < sueldoActual && sueldoActual <= 15000) {
   (nuevoSueldo = sueldoActual*1.2);
   console.log("Tiene un aumento del 20%, su sueldo actualizado es", nuevoSueldo);
-} else if (15001 < sueldoActual && sueldoActual <= 20000) {
-  nuevoSueldo = sueldoActual*1.1;
+  } else if (15001 <= sueldoActual && sueldoActual <= 20000) {
+  (nuevoSueldo = sueldoActual*1.1);
    console.log("Tiene un aumento del 10%, su sueldo actualizado es", nuevoSueldo);
-    } else if (20001 < sueldoActual && sueldoActual <= 25000) {
-  nuevoSueldo = sueldoActual*1.05;
+    } else if (20001 <= sueldoActual && sueldoActual <= 25000) {
+  (nuevoSueldo = sueldoActual*1.05);
     console.log("Tiene un aumento del 5%, su sueldo actualizado es", nuevoSueldo);
       } else {
-  nuevoSueldo = sueldoActual;
+  (nuevoSueldo = sueldoActual);
   console.log("Usted no posee aumento");
-} 
+  }
+//No me estaria saliendo por consola el sueldo actualizado(nuevoSueldo) 
--- src/paroimpar.ts
+++ src/paroimpar.ts
@@ -1,12 +1,12 @@
 //Desarrollar un algorit 
-let numero: number = Number(prompt("Ingrese numero"));
+//let numero: number = Number(prompt("Ingrese numero"));
 
-switch numero {
+//switch numero {
 
-  case numero % 2 === 0:
+ // case numero % 2 === 0:
   console.log("El numero es par")
   break;
-  case numero % 2 != 0:
+//  case numero % 2 != 0:
     console.log("El numero es impar")
 //    default:
 //      console.log("")
