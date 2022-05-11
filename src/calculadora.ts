c4db020aa382e8f8ea3b07247358635498003d8f
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-05-10 22:05:41 -0300

    ejercicio calculadora

---
+++ src/calculadora.ts
@@ -0,0 +1,27 @@
+function dibujarGuion() {
+  let guion: string = "-";
+  for (let contador: number = 1; contador <= 40; contador++) {
+    guion = guion + "-";
+  }
+  console.log(guion);
+}
+
+let numero1: number = Number(prompt("Ingrese numero 1"));
+let numero2: number = Number(prompt("Ingrese numero 2"));
+let opcion: number = Number(
+  prompt("Ingrese 1 para sumar, 2 para restar o cualquier otro para salir")
+);
+let resultado: number = 0;
+
+if (opcion === 1) {
+  resultado = numero1 + numero2;
+  dibujarGuion();
+  console.log("Resultado de la suma es:", resultado);
+  dibujarGuion();
+} else if (opcion === 2) {
+  resultado = numero1 - numero2;
+  dibujarGuion();
+  console.log("El resultado de la resta es:", resultado);
+  dibujarGuion();
+}
+//agregar un else opcion invalida
