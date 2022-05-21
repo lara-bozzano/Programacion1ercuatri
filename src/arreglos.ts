722b934e4c3af38758e7ea06dae3ebcb5ccbd057
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-05-20 21:58:11 -0300

    arreglos new array

---
+++ src/arreglos.ts
@@ -0,0 +1,9 @@
+let arregloNum: number[] = [];
+
+console.log(arregloNum[20]);
+console.log(arregloNum[14]);
+console.log(arregloNum[8]);
+console.log(arregloNum[0]);
+console.log(arregloNum[5]);
+console.log(arregloNum[19]);
+console.log(arregloNum[0]);
