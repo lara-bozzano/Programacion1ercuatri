33c43e704286ba64cb0d7c4c9bc2d32548e56e57
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-05-24 18:24:15 -0300

    esDivisor

--- src/esDivisor.ts
+++ src/esDivisor.ts
@@ -2,28 +2,9 @@
 //ej: para el número 16, sus divisores son 1, 2, 4, 8, 16, cantdivisores 5
 //Re-utilice el método esMultiplo implementado para el ejercicio anterior
 
-/*function cantidadDivisores(numero: number,divisor: number, cantidad:number) : number {
-  let divisor: number
-  let numero: number;
-  for () {
-  if (dividendo % indice === 0) {
-    contador = contador + 1:
-  } else { 
-    return 
-  }
-  return contador 
-}*/
-function esMultiplo(dividendo: number, divisor: number): boolean {
-  if (dividendo % divisor === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function cantidadDeDivisores(numero: number): number {
-  let cantidad: number = 0;
+function cantidadDivisores(numero: number): number {
   let divisor: number;
+  let cantidad: number = 0;
   for (divisor = 0; divisor <= numero; divisor++) {
     if (esMultiplo(numero, divisor)) {
       cantidad++;
@@ -32,6 +13,15 @@ function cantidadDeDivisores(numero: number): number {
   return cantidad;
 }
 
-let numero: number = Number(prompt("Ingrese un numero"));
-let numDivisores: number = cantidadDeDivisores(numero);
+function esMultiplo(dividendo: number, divisor: number): boolean {
+  if (dividendo % divisor === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let numero: number = Number(prompt("Ingrese un numero entero"));
+let numDivisores: number = cantidadDivisores(numero);
+
 console.log("El número", numero, "tiene ", numDivisores, "divisores.");
