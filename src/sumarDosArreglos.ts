a7be16d08df3ee6b54f3af033f9f98987dced3e7
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-05-24 22:05:38 -0300

    sumardos arreglos con funciones

--- src/sumarDosArreglos.ts
+++ src/sumarDosArreglos.ts
@@ -3,6 +3,21 @@ de dos arreglos y guardar el resultado en otro arreglo
 el arreglo tiene dimensión 6 y los números de los dos
 vectores los carga el usuario*/
 
+function cargarArreglo(numero1: [number]): void {
+  for (let indice = 0; indice < 6; indice++) {
+    numero1[indice] = Number(prompt("Ingrese el primer numero"));
+  }
+}
+
+function sumarArreglos(arreglo1: [number], arreglo2: [number], arreglo3: [number]): void {
+  for (let indice = 0; indice < 6; indice++) {
+  arreglo3[indice] = arreglo1 [indice] + arreglo2 [indice]
+  }
+
+function mostrarArreglos
+
+
+
 let numero1: number[] = new Array(6);
 let numero2: number[] = new Array(6);
 
