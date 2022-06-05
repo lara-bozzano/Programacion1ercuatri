2aaefe82be8be171926d54d9a9f56c5ac7ef5fad
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-06-05 19:24:22 -0300

    ESPAR

---
+++ src/esPar.ts
@@ -0,0 +1,11 @@
+let numero: number = Number(prompt("Ingrese un numero"));
+
+if (numero % 2 === 0) {
+  console.log("El num ingresado es par");
+} else {
+  console.log("El num ingresado es impar");
+}
+
+if (numero <= 0) {
+  numero = Number(prompt("Ingrese un num mayor a 0"));
+}
