28061eeb56682c41df01b11b0687180b961bce3c
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-06-07 01:03:54 -0300

    practica

---
+++ src/practica.ts
@@ -0,0 +1,11 @@
+/*let posicion: number = 10;
+while (posicion > 0) {
+  console.log("posicion ", posicion);
+  posicion = posicion - 1;
+} */
+
+let arregloNombres: string[] = ["braian", "maria", "fernando", "sofia"];
+
+for (let i: number = 0; i < arregloNombres.length; i++) {
+  console.log(arregloNombres[i]);
+}
