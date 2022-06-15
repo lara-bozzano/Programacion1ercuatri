ab26c09f860113e1c8f65d9502cd11e07d192b28
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-06-14 21:45:46 -0300

    promedio escolar

---
+++ src/promedioEscolar.ts
@@ -0,0 +1,14 @@
+//algoritmo q permita cargar alumnos y sus notas en los 3 trim
+//obtener el promedio anual (es decir, de sus tres notas)
+//del alumno ingresado por el usuario
+
+let nota1: number;
+let nota2: number;
+let nota3: number;
+
+let promedio
+let suma
+
+ for (let alumno: string = prompt("Ingrese nombre del alumno") {
+
+ }
