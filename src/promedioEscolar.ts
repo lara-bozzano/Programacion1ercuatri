e8a0e7577b5249717278aaefda94f3409f729dc1
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-06-18 21:05:12 -0300

    promedio escolar

--- src/promedioEscolar.ts
+++ src/promedioEscolar.ts
@@ -2,13 +2,18 @@
 //obtener el promedio anual (es decir, de sus tres notas)
 //del alumno ingresado por el usuario
 
-let nota1: number;
-let nota2: number;
-let nota3: number;
+let cantAlumnos: number = Number(prompt("Ingrese cantidad de alumnos:"));
+let alumnos: string[] = new Array(cantAlumnos);
+let nota1: number[] = new Array(cantAlumnos);
+let nota2: number[] = new Array(cantAlumnos);
+let nota3: number[] = new Array(cantAlumnos);
 
-let promedio
-let suma
-
- for (let alumno: string = prompt("Ingrese nombre del alumno") {
-
- }
+for (let indice = 0; indice < cantAlumnos; indice++) {
+  alumnos[indice] = prompt("Nombre del alumno: ");
+  nota1[indice] = Number(prompt("Nota 1er trimestre:"));
+  nota2[indice] = Number(prompt("Nota 2do trimestre:"));
+  nota3[indice] = Number(prompt("Nota 3er trimestre:"));
+  let promedio: number = 0;
+  promedio = (nota1[indice] + nota2[indice] + nota3[indice]) / 3;
+  console.log("El promedio de", alumnos[indice], "es", promedio);
+}
