76ff3d46c6d8753999fe7a5aa635e7da5b477005
Lara Bozzano <104174013+lara-bozzano@users.noreply.github.com>
2022-06-20 20:00:21 -0300

    sfga

--- src/tpCompra.ts
+++ src/tpCompra.ts
@@ -1,22 +1,37 @@
-//ingresar CANTIDADdeProds que va a llevar, 
+//ingresar CANTIDADdeProds que va a llevar,
 //cuales y ctos(cantidad) PRODUCTOS de c/u y
-//el PRECIO UNITARIO de cada producto 
+//el PRECIO UNITARIO de cada producto
 //mostrar: detalle de la compra: prods, cant, precio unitario y precio total.
 
-let cantTotalProd: number = Number(prompt("Ingrese cantidad total de productos:"));
+let cantTotalProd: number = Number(
+  prompt("Ingrese cantidad total de productos:")
+);
 let productos: string[] = new Array(cantTotalProd);
-let cantCadaProd: number = Number(prompt("Ingrese ctos compro de este prod"))
-let cantProds:number[] = new Array(cantCadaProd);
-
-let precioProducto: number = Number(prompt("ingresar el precio"));
-let cantidad: number = Number(prompt("ingresar cantidad"));
-let precioTotal: number = precioProducto * cantidad;
+//let cantCadaProd: number = Number(prompt("Ingrese ctos compro de este prod"))
+let cantProds: number[] = new Array(cantCadaProd);
 
 let ingresarCantProd;
-for (let i = 0; i <= cantTotalProd; i++) {
-  productos[i] = prompt("Ingrese los productos de la compra:");
-  cantProds[i] = Number(prompt("Que cantidad compro de este producto?"))
-  for (productos[]= )
-console.log("Cant total de prods: ", cantTotalProd, ". De los cuales:", cantProds[i], "son", productos[i]);
+while (cantProds === cantTotalProd) {
+  for (let i = 0; i <= cantTotalProd; i++) {
+    productos[i] = prompt("Ingrese los productos de la compra:");
+    let cantCadaProd: number = Number(
+      prompt("Ingrese ctos compro de este prod")
+    );
+    cantProds[i] = cantCadaProd;
+    console.log(
+      "Cant total de prods: ",
+      cantTotalProd,
+      ". De los cuales:",
+      cantProds[i],
+      "son",
+      productos[i]
+    );
+  }
 }
 
+/*
+let precioProducto: number = Number(prompt("ingresar el precio"));
+//let cantidad: number = Number(prompt("ingresar cantidad"));
+let precioTotal: number = 0;
+precioTotal = precioProducto * cantProds[] ;
+*/
